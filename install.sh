fc00db2772c9712790d72900978d272cb76277c3
victor <gictorbit@gmail.com>
2019-10-20 22:33:54 +0330

    edit for code quality

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/bash
 
 echo "creating cpaneltop directory in ~/.locale/share/ ..."
-mkdir -p $HOME/.local/share/cpaneltop
+mkdir -p "$HOME/.local/share/cpaneltop"
 
 echo "copy app directory to cpaneltop directory ..."
-cp -rv ./app $HOME/.local/share/cpaneltop
+cp -rv "./app $HOME/.local/share/cpaneltop"
 
 chmod +x $HOME/.local/share/cpaneltop/app/cpaneltop.py
 
 echo "make symbolic link for cpaneltop.py"
-sudo ln -s $HOME/.local/share/cpaneltop/app/cpaneltop.py /usr/local/bin/cpaneltop
+sudo ln -s "$HOME/.local/share/cpaneltop/app/cpaneltop.py /usr/local/bin/cpaneltop"
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 if [ -d "$HOME/.local/share/cpaneltop" ] then;
-    rm -rf $HOME/.local/share/cpaneltop
+    rm -rf "$HOME/.local/share/cpaneltop"
 else
     echo "there is no cpaneltop directory in ~/.local/share"
 fi
 
-sudo unlink /usr/local/bin/cpaneltop
+sudo unlink "/usr/local/bin/cpaneltop"
